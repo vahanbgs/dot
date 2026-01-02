3e2c7a68a49ab2d06d1ed3f22a9f1be400935daa
vahanbgs <51419074+vahanbgs@users.noreply.github.com>
2026-01-02 18:47:25 +0100

    feat: implement templates with placeholders for config files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use clap::{Parser, Subcommand, command};
 use directories_next::BaseDirs;
-use std::{fs, io, path::Path};
+use std::{
+    collections::HashMap,
+    fs::{self, File, read_to_string},
+    io::{self, Write},
+    path::{Path, PathBuf},
+};
+use tielpmet::template::Template;
 use walkdir::WalkDir;
 
 #[derive(Parser)]
@@ -15,7 +21,7 @@ enum Commands {
     Deploy,
 }
 
-pub fn copy(src: &Path, dst: &Path) -> io::Result<()> {
+pub fn deploy(src: &Path, dst: &Path) -> io::Result<()> {
     for entry in WalkDir::new(src).follow_links(false) {
         let entry = entry?;
         let src_file_path = entry.path();
@@ -35,7 +41,52 @@ pub fn copy(src: &Path, dst: &Path) -> io::Result<()> {
                 fs::create_dir_all(parent)?;
             }
 
-            fs::copy(src_file_path, dst_file_path)?;
+            const TEMPLATE_FILE_EXTENSION: &'static str = ".tielpmet";
+
+            if entry
+                .file_name()
+                .to_str()
+                .unwrap()
+                .ends_with(TEMPLATE_FILE_EXTENSION)
+            {
+                let dst_file_path = PathBuf::from(
+                    dst_file_path
+                        .to_str()
+                        .unwrap()
+                        .strip_suffix(TEMPLATE_FILE_EXTENSION)
+                        .unwrap(),
+                );
+
+                let template_string = read_to_string(src_file_path).unwrap();
+
+                let template = Template::from_str(&template_string, "(<|[", "]|>)")
+                    .unwrap_or_else(|e| panic!("{}", e));
+
+                let variable_set = template.get_variable_set();
+
+                let mut variable_map = HashMap::<String, String>::new();
+
+                for &variable_name in variable_set {
+                    print!("{variable_name}: ");
+                    io::stdout().flush().unwrap();
+
+                    let mut input = String::new();
+
+                    io::stdin()
+                        .read_line(&mut input)
+                        .expect("Failed to read line");
+
+                    let input = input.trim();
+
+                    variable_map.insert(variable_name.to_string(), input.to_string());
+                }
+
+                let mut output_file = File::create(&dst_file_path).unwrap();
+
+                template.render(&mut output_file, &variable_map).unwrap();
+            } else {
+                fs::copy(src_file_path, dst_file_path)?;
+            }
         }
     }
 
@@ -50,7 +101,7 @@ fn main() -> io::Result<()> {
     let src = base_dirs.data_dir().join("dot/home/");
     let dst = base_dirs.home_dir();
 
-    copy(&src, dst)?;
+    deploy(&src, dst)?;
 
     Ok(())
 }
